371ac0e59b950935324516e0b54e6890f3fda18d
rkotrys <robert.kotrys@gmail.com>
2021-11-26 15:40:14 +0100

    xxx

--- public/index.php
+++ public/index.php
@@ -89,9 +89,10 @@ function rpi_show($rpi){
    foreach( $rpi as $k=>$v){
        $buf.="<div class='flex-container  rpi-$k'>\n<div class='rpikey'>$k :</div>\n<div class='rpivalue'>$v</div>\n</div>\n";
    }
-   if( $tdif <4 ){
+
+   if( $tdif < 4 ){
       $buf.="<div style='width:100%;text-align:center;'>ON-Line</div>\n";
-   }esle{
+   }else{
       $buf.="<div style='width:100%;text-align:center;'>OFF-Line: ".$tdiff/60.0." min</div>\n";
    }
    
