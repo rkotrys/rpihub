3918c7391bbc628c1140f52209c20d86b3e1a37f
rkotrys <robert.kotrys@gmail.com>
2021-11-28 09:56:32 +0100

    log

--- public/index.php
+++ public/index.php
@@ -72,7 +72,7 @@ public function update($d, $key='sn'){
       $values.=" $k=".'"'.$v.'"'.",";
    }
    $values[strlen($values)-1]=' ';
-   $query="update rpi set $values where $key=".'"'.$d[$key].'",';
+   $query="update rpi set $values where $key=".'"'.$d[$key].'"';
    error_log( "UPDATE: $query", 3, "/srv/www/rpi/error.log" );
    try{ $r = $this->db->query($query); }
    catch(PDOException $e){ error_log( $e->getMessage().": ".$e->getCode()."\nQuery: $query", 3, "/srv/www/rpi/error.log" ); exit; }
