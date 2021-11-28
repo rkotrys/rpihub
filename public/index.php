3935ecefe0c1c567de0d591e29984f1e5f7ae66f
rkotrys <robert.kotrys@gmail.com>
2021-11-28 10:22:02 +0100

    xxx

--- public/index.php
+++ public/index.php
@@ -113,7 +113,7 @@ function rpi_show($rpi){
       if( $k=='model') continue;
        $buf.="<div class='flex-container  rpi-$k'>\n<div class='rpikey'>$k :</div>\n<div class='rpivalue'>$v</div>\n</div>\n";
    }
-   $buf.="<div class='rpi-status' >".($online=='rpi-online')?"ON-Line":"OFF-Line: ".totimedistance($tdiff)."</div>\n";
+   $buf.="<div class='rpi-status' >".(($online=='rpi-online')?"ON-Line":"OFF-Line: ".totimedistance($tdiff))."</div>\n";
    $buf.="</div>\n";
    return $buf;
 }
