e4f794f916a9b4f5525dad0296ca9ab0dea0627b
rkotrys <robert.kotrys@gmail.com>
2021-11-30 14:28:51 +0100

    xxx

--- public/view/edit_params.php
+++ public/view/edit_params.php
@@ -9,9 +9,9 @@
         <ol>
         <?php foreach( $faces as $face ) { ?>    
             <?php if( $face==$rpi['theme'] ){ ?>
-                <li><span style="background-color: <?=$face?>;color:<?=$face?>;box-shadow:2px 2px 3px #666;"> ***** </span> <a href='?get=theme&face=<?=$face?>&sn=<?=$rpi['sn']?>' ><?=$face?></a></li>
+                <li><span style="background-color: <?=$face?>;color:<?=$face?>;"> ***** </span> &nbsp;&nbsp;&nbsp;<?=$face?></li>
             <?php } else { ?>
-                <li><span style="background-color: <?=$face?>;color:<?=$face?>;"> ***** </span> <a href='?get=theme&face=<?=$face?>&sn=<?=$rpi['sn']?>' ><?=$face?></a> </li>
+                <li><span style="background-color: <?=$face?>;color:<?=$face?>;box-shadow:2px 2px 3px #666;"> ***** </span>  &nbsp;&nbsp;&nbsp;<a href='?get=theme&face=<?=$face?>&sn=<?=$rpi['sn']?>' ><?=$face?></a> </li>
             <?php } ?>    
         <?php } ?>    
         </ol>
