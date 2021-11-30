c454edac8c0eb06c56b6a7c51c685c7749b4d14f
rkotrys <robert.kotrys@gmail.com>
2021-11-30 12:36:13 +0100

    xxx

--- public/index.php
+++ public/index.php
@@ -256,18 +256,4 @@ if( isset($_GET['get']) and $_GET['get']!='' ){
 
    }
 }
-?><!DOCTYPE HTML>
-<html>
-<head>
-   <meta http-equiv='content-type' content='text/html; charset=utf-8'>
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
-   <link rel="stylesheet" href="css/rpi.css"> 
-</head>
-<body>
-   <header class="w3-container w3-teal">
-   <h1>RPI-hub</h1>
-   </header>      
-<?php if( isset($buf) ) print($buf); ?>
-</body>
-</html>
+view('main', array('content'=>$buf) );
