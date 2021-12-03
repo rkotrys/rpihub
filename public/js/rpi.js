e2c717c9046cb41b4bf6b129cbad29faaa68cfc5
rkotrys <robert.kotrys@gmail.com>
2021-12-03 13:17:14 +0100

    xxx

--- public/js/rpi.js
+++ public/js/rpi.js
@@ -2,10 +2,10 @@ $(document).ready(function(){
 
     function update_online(){
         $.getJSON("http://rpi.ontime24.pl/?get=isonline", function(result){
+            $(".rpi-online").removeClass("rpi-online");
+            $(".rpi-offline").removeClass("rpi-offline");
             $.each(result, function( i, rpi ){ 
-                  $(".rpi-online").removeClass("rpi-online");
-                  $(".rpi-offline").removeClass("rpi-offline");
-                  $("[sn="+rpi.sn+"]").addClass("rpi-"+rpi.online);
+                  $("[sn="+rpi.online+"]").addClass("rpi-"+rpi.online);
                 });               
         });
     }
