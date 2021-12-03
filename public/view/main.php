75a117fca48f89f19cf35071640d3325280a72e9
rkotrys <robert.kotrys@gmail.com>
2021-12-03 11:32:10 +0100

    xxx

--- public/view/main.php
+++ public/view/main.php
@@ -4,11 +4,13 @@
    <meta http-equiv='content-type' content='text/html; charset=utf-8'>
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
+   <script src="js/jquery-3.6.0.min.js"></script>
    <link rel="stylesheet" href="css/rpi.css"> 
+   <script src="js/rpi.js"></script>
    <script>
-       function do_onload(){
-          let myVar = setTimeout( function () { let timer = window.location.reload(true); }, 5000);
-       }
+   //    function do_onload(){
+   //       let myVar = setTimeout( function () { let timer = window.location.reload(true); }, 5000);
+   //    }
    </script>    
 </head>
 <body <?php if($do_reload) print( 'onload="do_onload()"' ); ?> >
