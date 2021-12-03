2846053878a6539f6f4cc1de8357676267290300
rkotrys <robert.kotrys@gmail.com>
2021-12-03 13:35:27 +0100

    xxx

--- public/js/rpi.js
+++ public/js/rpi.js
@@ -7,6 +7,9 @@ $(document).ready(function(){
             $.each(result, function( i, rpi ){ 
                   $("[sn="+rpi.sn+"]").addClass("rpi-"+rpi.online);
                   $("[sn="+rpi.sn+"] .rpi-last .rpivalue").text(rpi.last);
+                  $("[sn="+rpi.sn+"] .rpi-hostname .rpivalue").text(rpi.hostname);
+                  $("[sn="+rpi.sn+"] .rpi-ip .rpivalue").text(rpi.ip);
+                  $("[sn="+rpi.sn+"] .rpi-wip .rpivalue").text(rpi.wip);
                 });               
         });
     }
