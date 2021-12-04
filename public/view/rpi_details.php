0c555aa1616c985ca3362d6104376649b42dd15c
rkotrys <robert.kotrys@gmail.com>
2021-12-04 13:19:41 +0100

    xxx

--- public/view/rpi_details.php
+++ public/view/rpi_details.php
@@ -8,10 +8,27 @@
 ?>
 <div class='w3-card rpi ' sn='<?=$rpi["sn"]?>' >
 <div class='rpi-header'><?=$rpi['model']?></div>
-<?php foreach( $rpi as $k=>$v) { if( $k=='model') continue; ?>
+<div class='flex-container  rpi-sn'>
+        <div class='rpikey'>sn :</div>
+        <div class='rpivalue'><?=$rpi['sn']?></div>
+</div>
+<div class='flex-container  rpi-hostname'>
+        <div class='rpikey'>hostname :</div>
+        <div class='rpivalue'><?=$rpi['hostname']?></div>
+</div>
+<div class='flex-container  rpi-ip'>
+        <div class='rpikey'>sn :</div>
+        <div class='rpivalue'><?=$rpi['ip']?></div>
+</div>
+<div class='flex-container  rpi-wip'>
+        <div class='rpikey'>sn :</div>
+        <div class='rpivalue'><?=$rpi['wip']?></div>
+</div>
+
+<?php foreach( $rpi as $k=>$v) { if( $k=='model' or $k=='sn'  or $k=='hostname' or $k=='ip' or $k=='wip' ) continue; ?>
     <div class='flex-container  rpi-<?=$k?>'>
         <div class='rpikey'><?=$k?> :</div>
-        <div class='rpivalue'><?=($k!='cmd')?$v:'***';?></div>
+        <div class='rpivalue'><?=$v?></div>
     </div>
 <?php  } ?>
     <div class='rpi-status' >
