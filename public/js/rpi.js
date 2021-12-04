b359b17b5972e1555a36bdae8fcae59acdbc7f79
rkotrys <robert.kotrys@gmail.com>
2021-12-04 13:02:37 +0100

    xxx

--- public/js/rpi.js
+++ public/js/rpi.js
@@ -5,13 +5,12 @@ $(document).ready(function(){
             $(".rpi-online").removeClass("rpi-online");
             $(".rpi-offline").removeClass("rpi-offline");
             let show_offline = ( $("#hide_offline").prop("checked") )?true:false;
-            console.log( "checked:", $("#hide_offline").prop("checked"), "\nshow_offline: "+show_offline );
             $.each(result, function( i, rpi ){ 
                   $("[sn="+rpi.sn+"]").addClass("rpi-"+rpi.online);
-                  //if( show_offline )  
-                  //  $("[sn="+rpi.sn+"]").show();
-                  //else 
-                  //  $(".rpi-offline[sn="+rpi.sn+"]").hide();
+                  if( show_offline )  
+                    $("[sn="+rpi.sn+"]").show();
+                  else 
+                    $(".rpi-offline[sn="+rpi.sn+"]").hide();
                   $("[sn="+rpi.sn+"] .rpi-last .rpivalue").text(rpi.last);
                   $("[sn="+rpi.sn+"] .rpi-hostname .rpivalue").text(rpi.hostname);
                   $("[sn="+rpi.sn+"] .rpi-ip .rpivalue").text(rpi.ip);
