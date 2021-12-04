645b8bd9cae63d29be2b2f14aa7c98d2c2f62763
rkotrys <robert.kotrys@gmail.com>
2021-12-04 11:26:43 +0100

    xxx

--- public/view/rpi_list.php
+++ public/view/rpi_list.php
@@ -1,7 +1,7 @@
 <div class='w3-container'>
     <header class="w3-display-container">
         <h2 class="w3-left">List of RPi devices</h2>
-        <div class="w3-right w3-card" ><div style="padding:5px;margin-top: 5px;"><input type="checkbox" id="hide_offline" name="hide_offline" value="hide_offline"> show all</div></div>
+        <div class="w3-right w3-card" ><div style="padding:6px 12px;"><input type="checkbox" id="hide_offline" name="hide_offline" value="hide_offline"> show all</div></div>
     </header>
     <div class='rpi-list'>
     <?php 
