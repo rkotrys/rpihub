0135a747db47a04b90a08492da95e5eea9500527
rkotrys <robert.kotrys@gmail.com>
2021-12-04 13:52:03 +0100

    xxx

--- public/view/rpi_details.php
+++ public/view/rpi_details.php
@@ -33,9 +33,9 @@
 <?php  } ?>
     <div class='flex-container rpi-status' >
     <?php if($online){ ?>
-        <a class="w3-btn w3-blue w3-round w3-small" href='?get=edit&sn=<?=$rpi['sn']?>'>configure</a>
+        <a class="w3-button w3-blue w3-round w3-small" href='?get=edit&sn=<?=$rpi['sn']?>'>configure</a>
     <?php }else{ ?>
-        <a class="w3-btn w3-red w3-round w3-small" href='http://rpi.ontime24.pl/?get=delete&sn=<?=$rpi["sn"]?>' >remove</a>
+        <a class="w3-button w3-red w3-round w3-small" href='http://rpi.ontime24.pl/?get=delete&sn=<?=$rpi["sn"]?>' >remove</a>
     <?php } ?>   
     </div>
 </div>
