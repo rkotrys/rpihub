6b2da57dbd075100209d69b14e046ab52ae54dd2
rkotrys <robert.kotrys@gmail.com>
2021-12-05 11:40:44 +0100

    xxx

--- public/view/rpi_list.php
+++ public/view/rpi_list.php
@@ -1,15 +1,15 @@
 <div class='w3-container'>
-    <header class="w3-display-container">
-        <h2 class="w3-left">List of RPi devices</h2>
-        <div class="w3-right w3-card" ><div style="padding:6px 12px;">
-           <input type="checkbox" id="hide_offline" name="hide_offline" value="hide_offline"> show all</div>
-        </div>
-    </header>
-    <div class='rpi-list'>
-    <?php 
+<header class="w3-display-container">
+<h2 class="w3-left">List of RPi devices</h2>
+<div class="w3-right w3-card" ><div style="padding:6px 12px;">
+<input type="checkbox" id="hide_offline" name="hide_offline" value="hide_offline"> show all</div>
+</div>
+</header>
+<div class='rpi-list'>
+<?php 
        foreach( $rpi_list as $sn=>$rpi ){
           echo view( 'rpi_details', array( 'rpi'=>$rpi ) );
        }       
-    ?>   
-    </div>
+?>   
+</div>
 </div>  
