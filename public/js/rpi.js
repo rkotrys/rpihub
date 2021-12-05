e1b9ba00bc69d5bd53a55c363c40c210f5b14846
rkotrys <robert.kotrys@gmail.com>
2021-12-05 12:10:14 +0100

    xxx

--- public/js/rpi.js
+++ public/js/rpi.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
                   if( rpi.online=='online'){
                     $(".rpi[sn='"+rpi.sn+"'] a[status='configure']" ).show();
                   }else{
-                    $(".rpi[sn='"+rpi.sn+"'] a[starus='configure']" ).hide();
+                    $(".rpi[sn='"+rpi.sn+"'] a[status='configure']" ).hide();
                   }
 
                   if( show_offline | rpi.online=='online' )  
