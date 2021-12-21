86b8ba49733b564c3b690ef2542f1f47a94aa8f4
rkotrys <robert.kotrys@gmail.com>
2021-12-21 14:32:21 +0100

    xxx

--- public/index.php
+++ public/index.php
@@ -225,7 +225,8 @@ if( isset($_GET['get']) and $_GET['get']!='' ){
             unset( $rpi['cmd'] );
             $ld =  json_decode( base64_decode( $rpi['theme'] ), true );
             //$rpi['theme']=($ld['display']=='oled13')?'mono':$ld['localdata']['theme'];
-            $rpi['ld']=$ld;
+            $rpi['ld']=$ld['localdata'];
+            $rpi['display']=$ld['display'];
             $r[$sn]=$rpi;
          }   
          header("Content-Type: application/json; charset=UTF-8");
