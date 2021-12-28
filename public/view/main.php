0c86c1a3a06e9c5eff9b49c5f2942b0fd099ebd7
rkotrys <robert.kotrys@gmail.com>
2021-12-28 13:43:48 +0100

    xxx

--- public/view/main.php
+++ public/view/main.php
@@ -3,6 +3,9 @@
 <head>
    <meta http-equiv='content-type' content='text/html; charset=utf-8'>
    <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+   <meta http-equiv="Pragma" content="no-cache" />
+   <meta http-equiv="Expires" content="0" />   
    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
    <script src="js/jquery-3.6.0.min.js"></script>
    <link rel="stylesheet" href="css/rpi.css"> 
