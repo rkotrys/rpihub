5a0a1e1d205183bf7e7b2c19135ff3230cec35fb
rkotrys <robert.kotrys@gmail.com>
2021-12-28 14:15:32 +0100

    xxx

--- public/view/edit_params.php
+++ public/view/edit_params.php
@@ -59,7 +59,7 @@
         </header>
         <div class="w3-center" >
             <form id="wlan_client" method="get" action="">
-            <input class="w3-input" name="essid" id="essid" type="text" value="<?=$ld['essid'] ?>" placeholder="WLAN ESSID" >
+            <input class="w3-input" name="essid" id="essid" type="text" value="<?=$ld['localdata']['essid'] ?>" placeholder="WLAN ESSID" >
             <input class="w3-input" name="wlan_key" id="wlan_key" type="text" value="" placeholder="wpa-key">
             <input type="hidden" name="set" value="wlan_client" >
             <input type="hidden" name="sn" value="<?=$rpi['sn']?>" >
