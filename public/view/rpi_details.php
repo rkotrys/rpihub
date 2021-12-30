5436f802e032dfac3a77b911e4470b472ed2588e
rkotrys <robert.kotrys@gmail.com>
2021-12-30 10:50:56 +0100

    xxx

--- public/view/rpi_details.php
+++ public/view/rpi_details.php
@@ -32,6 +32,14 @@
         <div class='rpivalue'><?=$ld['localdata']['essid']?></div>
 </div>
 <?php } ?>
+<div class='flex-container  rpi-puuid'>
+        <div class='rpikey'>puuid :</div>
+        <div class='rpivalue'><?=$rpi['puuid']?></div>
+</div>
+<div class='flex-container  rpi-msdid'>
+        <div class='rpikey'>msdid :</div>
+        <div class='rpivalue'><?=$ld['localdata']['msdid']?></div>
+</div>
 <div class='flex-container  rpi-ip'>
         <div class='rpikey'>eth0 ip :</div>
         <div class='rpivalue'><?=$rpi['ip']?></div>
@@ -40,11 +48,19 @@
         <div class='rpikey'>wlan0 ip :</div>
         <div class='rpivalue'><?=$rpi['wip']?></div>
 </div>
+<div class='flex-container  rpi-ip'>
+        <div class='rpikey'>eth0 MAC :</div>
+        <div class='rpivalue'><?=$rpi['emac']?></div>
+</div>
+<div class='flex-container  rpi-wip'>
+        <div class='rpikey'>wlan0 MAC :</div>
+        <div class='rpivalue'><?=$rpi['wmac']?></div>
+</div>
 <div class='flex-container  rpi-arch'>
         <div class='rpikey'>arch :</div>
         <div class='rpivalue'><?=$rpi['arch']." ".$ld['localdata']['cpus'].' cpu(s)'?></div>
 </div>
-<?php foreach( $rpi as $k=>$v) { if( $k=='model' or $k=='sn'  or $k=='hostname' or $k=='ip' or $k=='wip' or $k=='cmd' or $k=='arch' ) continue; ?>
+<?php foreach( $rpi as $k=>$v) { if( $k=='model' or $k=='sn'  or $k=='hostname' or $k=='ip' or $k=='wip' or $k=='cmd' or $k=='arch' or $k=='wmac' or $k=='wmac' or $k=='puuid' ) continue; ?>
     <div class='flex-container  rpi-<?=$k?>'>
         <div class='rpikey'><?=$k?> :</div>
         <div class='rpivalue'><?=$v?></div>
