c93a13d67aed5675a02b4ba15abb9a5f10c9c8d1
rkotrys <robert.kotrys@gmail.com>
2021-12-30 11:33:44 +0100

    xxx

--- public/view/rpi_details.php
+++ public/view/rpi_details.php
@@ -38,7 +38,7 @@
 <?php } ?>
 <div class='flex-container  rpi-wlans'>
         <div class='rpikey'>wlans :</div>
-        <div class='rpivalue'><?=$ld['licaldata']['wlans']?></div>
+        <div class='rpivalue'><?=$ld['localdata']['wlans']?></div>
 </div>
 <div class='flex-container  rpi-puuid'>
         <div class='rpikey'>PUUID :</div>
