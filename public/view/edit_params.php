ca38fbe70cc494af123a8e123238c341f7dbcfb0
rkotrys <robert.kotrys@gmail.com>
2021-12-31 10:52:00 +0100

    xxx

--- public/view/edit_params.php
+++ public/view/edit_params.php
@@ -68,6 +68,7 @@
         print( print_r( $ld['localdata']['scan']['IR2022'], TRUE) );
         foreach( $ld['localdata']['scan'] as $k=>$v )  { $wlans[$k] = $v['level']; }
         asort($wlans);
+        $wlans=array_reverse($wlans);
         print( print_r($wlans, TRUE) );
         foreach(  $wlans as $k=>$v){  ?>
         <tr><td>"<?=$k?>"</td><td class="w3-center"><?=$ld['localdata']['scan']['channel']?></td><td><?=$ld['localdata']['scan']['level']?>dB</td></tr>
