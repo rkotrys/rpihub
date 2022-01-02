57a3f4a0af872a5014819f360bc19a16064d0b51
rkotrys <robert.kotrys@gmail.com>
2022-01-02 15:21:28 +0100

    xxx

--- public/js/rpi_edit_params.js
+++ public/js/rpi_edit_params.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
    $(".wlan_name").click( function(){
-       alert($(this).attr("wlan_name")+'->'+$("input[name='essid']").val());
        $("input[name='essid']").val( $(this).attr("wlan_name"));
    });
 
