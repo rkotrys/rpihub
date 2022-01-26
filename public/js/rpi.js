7489e44bdaf19547cf1291a2c0b597ece82c8663
rkotrys <robert.kotrys@gmail.com>
2022-01-26 13:09:24 +0100

    xxx

--- public/js/rpi.js
+++ public/js/rpi.js
@@ -27,11 +27,11 @@ $(document).ready(function(){
                     $("[sn="+rpi.sn+"] .rpi-wip .rpivalue").text(rpi.wip);
                     if( rpi.ld.AP!=null ){
                       $("[sn="+rpi.sn+"] .rpi-AP .rpivalue").text("AP '"+rpi.ld.AP.ssid+"'");
-                      $("[sn="+rpi.sn+"] .rpi-AP .rpikey").text("BSSID");
-                      $("[sn="+rpi.sn+"] .rpi-AP .rpivalue").text(rpi.ld.AP.bssid);
+                      $("[sn="+rpi.sn+"] .rpi-essid .rpikey").text("BSSID");
+                      $("[sn="+rpi.sn+"] .rpi-essid .rpivalue").text(rpi.ld.AP.bssid);
                     }else{
                       $("[sn="+rpi.sn+"] .rpi-AP .rpivalue").text("Station");
-                      $("[sn="+rpi.sn+"] .rpi-AP .rpikey").text("ESSID");
+                      $("[sn="+rpi.sn+"] .rpi-essid .rpikey").text("ESSID");
                     }
                     if( rpi.ld!=null ){
                       let t = rpi.ld.coretemp.valueOf();
